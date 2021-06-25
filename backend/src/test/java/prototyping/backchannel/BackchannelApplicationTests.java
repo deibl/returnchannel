c9779e4dc885089c45138d0b703769f19ae64eb1
David Eibl <david.eibl@prosiebensat1.com>
2021-06-25 11:01:30 +0200

    Init

---
+++ backend/src/test/java/prototyping/backchannel/BackchannelApplicationTests.java
@@ -0,0 +1,13 @@
+package prototyping.backchannel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackchannelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
